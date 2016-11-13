7601e8df5cccb99eb928f4c5a6df15182fc0eaf1
AIT EL ALIM Raphael <raphael.aitelalim@gmail.com>
2016-11-13 16:24:19 +0100

    page participer et programme

---
+++ dev/wp-content/themes/Bojack_Theme/page-templates/participer.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: Participer
+*/
+get_header(); //appel du template header.php  ?>
+<section class="container">
+    <div class="row bojackParticiper">
+
+        <h1 class="col-md-12">REJOINDRE AUTREMENT</h1>
+
+        <article class="col-md-6" id="bojackParticiper">
+           	<p>
+          		Les élections s’approchent à grands pas et les meetings se multiplient, BoJack traverse tout le nord de la France ces prochaines semaines pour y rencontrer des travailleurs des industries chimiques, des hôpitaux, des municipalités et de nombreux autres secteurs. 
+          		<br></br>
+          		Ces meetings et rencontrent sont ouverts à tous et vous permettrons d’en apprendre plus sur la campagne, mais aussi de rencontrer son équipe ainsi que M. Horseman qui répondra à vos questions.
+          		<br></br>
+				Notez où se trouve l’événement le plus proche de chez vous et n’hésitez pas à signaler à vos amis que vous participez !<br></br> A bientôt
+            </p>
+        </article>
+        <div class="map col-md-6">
+        	[powr-map id=18bbd0b0_1479049388]
+        </div>
+         <a href="participer" class="join">Participer</a>
+    </div>
+</section>
+<?php get_footer(); //appel du template footer.php ?>
